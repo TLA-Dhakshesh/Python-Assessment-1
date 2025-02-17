26f32389c15c3df7e44795249d4f3fe80cd81b05
TLA-Dhakshesh <129954679+TLA-Dhakshesh@users.noreply.github.com>
2025-02-17 12:31:32 +0530

    Add files via upload

---
+++ String_processing.py
@@ -0,0 +1,36 @@
+def longest(words):
+    m=0
+    w=[]
+    for j in words:
+        if m<len(j):
+            m=len(j)
+    for j in words:
+        if m==len(j):
+            w.append(j)
+    return w
+def palindrome(words):
+    l1=[]
+    for j in words:
+        if j.lower()==j.lower()[::-1] and len(j)>1:
+            l1.append(j)
+    return l1
+input_text = open("sentences.txt","r")
+Output=[]
+for i in input_text:
+    words=i.split()
+    count=len(words)
+    long=longest(words)
+    pal=palindrome(words)
+    if len(pal)>=1:
+        npal = ', '.join(pal)
+    else:
+        napl= "None"
+    final = (
+        f"Sentence: {i.strip()}\n"
+        f"Word Count: {count}\n"
+        f"Longest Word: {', '.join(long)}\n"
+        f"Palindromes: {napl}\n"
+    )
+    Output.append(final)
+Output_text=open("sentence_analysis.txt","w")
+Output_text.write('\n'.join(Output))
